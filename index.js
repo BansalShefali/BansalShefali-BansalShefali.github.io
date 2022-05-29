963e2fd68ff76205313767a59db12bcb4a1278ea
Shefali Bansal <80316273+BansalShefali@users.noreply.github.com>
2022-05-29 17:04:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+fetch('https://disease.sh/v3/covid-19/countries/india')
+.then((response) => {
+  return response.json();
+})
+.then((data) => {
+  console.log(data);
+    document.getElementById("active").innerHTML = data.active.toLocaleString();
+    document.getElementById("total").innerHTML = data.cases.toLocaleString();
+    document.getElementById("deaths").innerHTML = data.deaths.toLocaleString();
+    document.getElementById("recovered").innerHTML = data.recovered.toLocaleString();
+	document.getElementById("critical").innerHTML = data.recovered.toLocaleString();
+	document.getElementById("tests").innerHTML = data.recovered.toLocaleString();
+});
+
+// Scroll TO Top
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 100);
+			return false;
+		});
+});
+
+// Contact Tag
+$(document).ready(function(){
+	$('.toggle').click(function(){
+	  $('.sidebar-contact').toggleClass('active')
+	  $('.toggle').toggleClass('active')
+	})
+  })
+
+//Try
+
